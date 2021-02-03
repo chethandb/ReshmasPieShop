0bc9960641b76b5709bccb12888ed41dfdc030af
Chethan D. Balakrishna <Chethan.Balakrishna@biberk.com>
2021-02-02 22:12:06 -0600

    added relevant models

---
+++ ReshmasPieShop/Models/IPieRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReshmasPieShop.Models
+{
+    public interface IPieRepository
+    {
+        public IEnumerable<Pie> AllPies { get; }
+        public IEnumerable<Pie> PiesOfTheWeek { get; }
+        Pie GetPieById(int pieId);
+    }
+}
