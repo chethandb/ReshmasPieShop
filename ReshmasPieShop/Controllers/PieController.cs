7c0220a0e30b8e2d7c32591ac5e9116bfffefe23
Chethan D. Balakrishna <Chethan.Balakrishna@biberk.com>
2021-02-02 22:22:29 -0600

    added controller

---
+++ ReshmasPieShop/Controllers/PieController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ReshmasPieShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReshmasPieShop.Controllers
+{
+    public class PieController : Controller
+    {
+        private readonly IPieRepository _pieRepository;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public PieController(IPieRepository pieRepository,
+                             ICategoryRepository categoryRepository)
+        {
+            _pieRepository = pieRepository;
+            _categoryRepository = categoryRepository;
+        }
+
+        public ViewResult List()
+        {
+            return View(_pieRepository.AllPies);
+        }
+    }
+}
