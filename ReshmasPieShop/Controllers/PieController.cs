4aa16ca6e1a22f90f0aca1ad350c1754794b5ce2
Chethan D. Balakrishna <Chethan.Balakrishna@biberk.com>
2021-02-07 23:09:22 -0600

    added CategoryMenu view component

--- ReshmasPieShop/Controllers/PieController.cs
+++ ReshmasPieShop/Controllers/PieController.cs
@@ -20,14 +20,18 @@ namespace ReshmasPieShop.Controllers
             _categoryRepository = categoryRepository;
         }
 
-        public ViewResult List()
-        {
-            PiesListViewModel piesListViewModel = new PiesListViewModel();
-            piesListViewModel.Pies = _pieRepository.AllPies;
-            piesListViewModel.CurrentCategory = "Cheese cakes";
+        // GET: /<controller>/
+        //public IActionResult List()
+        //{
+        //    //ViewBag.CurrentCategory = "Cheese cakes";
 
-            return View(piesListViewModel);
-        }
+        //    //return View(_pieRepository.AllPies);
+        //    PiesListViewModel piesListViewModel = new PiesListViewModel();
+        //    piesListViewModel.Pies = _pieRepository.AllPies;
+
+        //    piesListViewModel.CurrentCategory = "Cheese cakes";
+        //    return View(piesListViewModel);
+        //}
 
         public IActionResult Details(int id)
         {
@@ -38,6 +42,30 @@ namespace ReshmasPieShop.Controllers
             }
 
             return View(pie);
+        }
+
+        public ViewResult List(string category)
+        {
+            IEnumerable<Pie> pies;
+            string currentCategory;
+
+            if (string.IsNullOrEmpty(category))
+            {
+                pies = _pieRepository.AllPies.OrderBy(p => p.PieId);
+                currentCategory = "All pies";
+            }
+            else
+            {
+                pies = _pieRepository.AllPies.Where(p => p.Category.CategoryName == category)
+                    .OrderBy(p => p.PieId);
+                currentCategory = _categoryRepository.AllCategories.FirstOrDefault(c => c.CategoryName == category)?.CategoryName;
+            }
+
+            return View(new PiesListViewModel
+            {
+                Pies = pies,
+                CurrentCategory = currentCategory
+            });
         }
     }
 }
